7784d3f962008aeeff342099de4bc6696221a0b7
sukesuke-code <79008036+sukesuke-code@users.noreply.github.com>
2021-03-21 17:51:13 +0900

    commit

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\exampleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/', function () {
+    return view('top');
+})->name('top');
+
+Route::get('/example', [exampleController::class, 'test']);
 
 Route::get('/', function () {
     return view('welcome');
